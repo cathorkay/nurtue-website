7fb7e48a4b05a72a8d93d1677e2a1babba425802
Rui Ying <yingrui205@gmail.com>
2022-02-15 21:32:48 -0800

    Add screenshots to home

--- components/Home.tsx
+++ components/Home.tsx
@@ -3,27 +3,36 @@ import Image from "components/Image";
 const Home: React.FC = () => {
   return (
     <section className="section bg-theme-lightblue">
-      <div className="h-full container mx-auto flex flex-col sm:flex-row items-center justify-center px-6 lg:px-12 sm:space-x-24 pt-14 sm:pt-16">
-        <Image
-          className="mx-auto w-11/12 h-auto sm:mx-0 sm:w-auto sm:h-96"
-          src="logo-image.png"
-          alt="logo"
-          width={1024}
-          height={1024}
-        />
-        <div className="flex flex-col items-center w-full h-2/5 sm:w-2/5 sm:h-auto text-theme-green text-center sm:text-left mt-8 sm:mt-0">
+      <div className="h-full container mx-auto flex flex-col sm:flex-row items-center justify-center sm:justify-start px-6 lg:px-12 pt-14 sm:pt-16">
+        <div className="flex flex-col items-center w-full sm:w-5/12 text-theme-green text-center">
           <Image
-            className="h-28 sm:h-48 object-contain"
+            className="mx-auto h-auto w-5/6 sm:h-48 sm:w-48"
+            src="logo-image.png"
+            alt="logo"
+            width={1024}
+            height={1024}
+          />
+          <Image
+            className="h-28 sm:h-48 object-contain mt-4 sm:mt-4"
             src="logo-text-phrase.png"
             alt="nurtue - grow together."
             width={1224}
             height={512}
           />
-          <div className="text-base sm:text-xl mt-6 sm:mt-12">
+          <div className="text-base text-center sm:text-xl mt-10 sm:mt-12 sm:w-9/12">
             Nurtue exists to improve relationships between parents and their
             children by promoting healthy communication habits and communal
             support.
           </div>
+        </div>
+        <div className="hidden sm:flex items-center fixed -z-10 inset-y-0 left-1/4 h-screen w-full">
+          <Image
+            className="h-full object-contain -mb-32"
+            src="home-screenshots.png"
+            alt="app screenshots"
+            width={2052}
+            height={1740}
+          />
         </div>
       </div>
     </section>
