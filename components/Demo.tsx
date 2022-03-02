fdf7010389158c49d67693bfc784e2b2ff29d7e0
Rui Ying <yingrui205@gmail.com>
2022-03-01 23:39:08 -0800

    Update Demo page

--- components/Demo.tsx
+++ components/Demo.tsx
@@ -2,12 +2,15 @@ const Demo: React.FC = () => {
   return (
     <section className="section bg-theme-red">
       <div className="h-full container mx-auto flex flex-col sm:flex-row items-center justify-center px-6 lg:px-28 pt-14 sm:pt-16">
-        <div className="mb-16 sm:mb-0 sm:w-1/3 text-white">
+        <div className="mb-16 w-full sm:mb-0 sm:w-1/3 text-white">
           <h2 className="text-2xl sm:text-5xl font-bold mb-4 sm:mb-12">Demo</h2>
           <div className="text-base sm:text-2xl">
-            [High-Fi Prototype Description] The quick brown fox jumps over the
-            lazy dog. The quick brown fox jumps over the lazy dog. The quick
-            brown fox jumps over the lazy dog.
+            <a
+              href="#design-process"
+              className="inline-block no-underline text-center rounded-2xl ring-2 ring-white bg-theme-red hover:bg-red-300 w-full sm:w-auto px-6 py-3 sm:px-16 sm:py-4"
+            >
+              Try Prototype
+            </a>
           </div>
         </div>
         <div className="w-full h-2/5 sm:w-2/3 ml-auto sm:ml-16 aspect-w-16 aspect-h-6">
