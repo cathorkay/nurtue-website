3e9d37a99f582b57e483bfb7fe67a4103e3269fa
Cat Horkay <76190287+cathorkay@users.noreply.github.com>
2022-07-27 15:54:53 -0700

    Added contact email

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,6 +2,7 @@ const Footer: React.FC = () => (
   <footer className="section fp-auto-height">
     <div className="w-full flex flex-col items-center p-6 bg-theme-green text-white text-sm text-center">
       <div>Copyright © 2022 Team Nurtue</div>
+      <div>hello@nurtue.app</div>
       <div className="mt-2">All rights reserved.</div>
     </div>
   </footer>
